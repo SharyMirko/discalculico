3a1c572576b3939ad9803bfd91543a015a37711f
Mirko Moschella <shary.mirko@gmail.com>
2022-02-21 15:32:06 +0100

    lo sconto era per gli over 65

--- js/sacript.js
+++ js/sacript.js
@@ -1,6 +1,6 @@
 const prezzoAlKm = 0.21; 
 const scontoMinorenni = 0.168;
-const scontoOver40 = 0.126;
+const scontoOver65 = 0.126;
 let eta = document.getElementById('eta').value;
 let km = document.getElementById('km').value;
 
@@ -9,7 +9,7 @@ if(eta < 18) {
     let prezzoscontato = km * scontoMinorenni;
     document.getElementById('costo').innerHTML = prezzoscontato.toFixed(2) + "€";
 } else if(eta >= 40) {
-    let prezzoscontato = km * scontoOver40;
+    let prezzoscontato = km * scontoOver65;
     document.getElementById('costo').innerHTML = prezzoscontato.toFixed(2) + " €";
 } else {
     let prezzotot = km * prezzoAlKm;
